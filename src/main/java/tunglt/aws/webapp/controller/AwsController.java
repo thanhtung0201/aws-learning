117416ddb7a5cf7643786155eb97bce179ca4605
Thanh Tung <thanhtung@MacBook-Pro-cua-Thanh.local>
2021-03-13 22:29:56 +0700

    Add log

--- src/main/java/tunglt/aws/webapp/controller/AwsController.java
+++ src/main/java/tunglt/aws/webapp/controller/AwsController.java
@@ -1,13 +1,18 @@
 package tunglt.aws.webapp.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
+@Slf4j
 public class AwsController {
 
     @GetMapping("/aws-index")
-    public String wellComeAws() {
+    public String wellComeAws(HttpServletRequest req) {
+        log.info("Request get from IP : {} , method {} : ", req.getRemoteAddr(), req.getMethod());
         return "Welcome aws web page";
     }
 
