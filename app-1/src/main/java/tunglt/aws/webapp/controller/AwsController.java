bdc342a79e12fce77e66ede4e788fb2099e00845
Thanh Tung <thanhtung@MacBook-Pro-cua-Thanh.local>
2021-03-17 20:32:15 +0700

    Restructure project

---
+++ app-1/src/main/java/tunglt/aws/webapp/controller/AwsController.java
@@ -0,0 +1,19 @@
+package tunglt.aws.webapp.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@Slf4j
+public class AwsController {
+
+    @GetMapping("web-app/aws-index")
+    public String wellComeAws(HttpServletRequest req) {
+        log.info("[Web app]Request get from IP : {} , method {} : ", req.getRemoteAddr(), req.getMethod());
+        return "Welcome aws web page of web app";
+    }
+
+}
